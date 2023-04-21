68b93f23239d7c5d0312747fcd1e8f2da1e6408e
sage <37631123+gamernewone@users.noreply.github.com>
2023-04-21 13:57:01 +0000

    mangodb first phase

--- server/controllers/fzController.js
+++ server/controllers/fzController.js
@@ -1,7 +1,19 @@
+require("../models/database");
+const Category = require("../models/Category");
 /* GET /
-* HOMEPAGE
-*/
+ * HOMEPAGE
+ */
 
 exports.home = async (req, res) => {
   res.render("index");
 };
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany();
+  } catch (error) {
+    console.log("error ", +error);
+  }
+}
+
+insertDummyCategoryData();
